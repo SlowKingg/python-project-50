9c3810ca4946b85a3f48f52fbf34529c7c56cde6
SlowKing <slowkinggg@vk.com>
2025-08-25 16:45:20 +0700

    refactor: extract argument parsing into a separate function

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,6 +2,16 @@ import argparse
 import json
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Compares two configuration files and shows a difference."
+    )
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
+    parser.add_argument("-f", "--format", help="set format of output")
+    return parser.parse_args()
+
+
 def read_files(args):
     file1 = json.load(open(args.first_file))
     file2 = json.load(open(args.second_file))
@@ -9,17 +19,10 @@ def read_files(args):
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description="Compares two configuration files and shows a difference."
-    )
-    parser.add_argument("first_file")
-    parser.add_argument("second_file")
-    parser.add_argument("-f", "--format", help="set format of output")
-
-    args = parser.parse_args()
+    args = parse_args()
 
     file1, file2 = read_files(args)
-    
+
     print(file1)
     print(file2)
 
