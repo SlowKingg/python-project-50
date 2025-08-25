143b2bc5eff6a063942fc03e675faf32e8dbbe6b
SlowKing <slowkinggg@vk.com>
2025-08-25 20:35:05 +0700

    chore: move expected diff to test_data

--- tests/test_diff_generator.py
+++ tests/test_diff_generator.py
@@ -7,13 +7,7 @@ def test_generate_diff_simple():
     file1 = json.load(open("tests/test_data/file1.json"))
     file2 = json.load(open("tests/test_data/file2.json"))
 
-    expected = (
-        "{\n- follow: False\n"
-        "  host: hexlet.io\n"
-        "- proxy: 123.234.53.22\n"
-        "- timeout: 50\n"
-        "+ timeout: 20\n"
-        "+ verbose: True\n}"
-    )
+    with open("tests/test_data/expected_diff.txt", "r") as f:
+        expected = f.read()
 
     assert generate_diff(file1, file2) == expected
