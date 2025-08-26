10bdbcc032b067294656c19afe8762cc95bfffcd
SlowKing <slowkinggg@vk.com>
2025-08-27 00:45:52 +0700

    refactor: :recycle: simplify variable assignment in main function

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,8 +3,11 @@ from gendiff import generate_diff, parse_args
 
 def main():
     args = parse_args()
+    file1_path = args.first_file
+    file2_path = args.second_file
+    format_name = args.format
 
-    print(generate_diff(args.first_file, args.second_file))
+    print(generate_diff(file1_path, file2_path, format_name))
 
 
 if __name__ == "__main__":
