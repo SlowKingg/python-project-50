2490b41cef54f403a70e54ce945650df82d68028
SlowKing <slowkinggg@vk.com>
2025-08-27 00:46:08 +0700

    test: :white_check_mark: add nested JSON and YAML diff generation tests

--- tests/test_diff_generator.py
+++ tests/test_diff_generator.py
@@ -19,3 +19,26 @@ def test_generate_diff_yaml():
         expected = f.read()
 
     assert generate_diff(file1, file2) == expected
+
+
+def test_generate_diff_nested_json():
+    file1 = "tests/test_data/file1_nested.json"
+    file2 = "tests/test_data/file2_nested.json"
+
+    with open("tests/test_data/expected_nested_diff.txt", "r") as f:
+        expected = f.read()
+
+    print(generate_diff(file1, file2))
+    print(expected)
+
+    assert generate_diff(file1, file2) == expected
+
+
+def test_generate_diff_nested_yaml():
+    file1 = "tests/test_data/file1_nested.yml"
+    file2 = "tests/test_data/file2_nested.yml"
+
+    with open("tests/test_data/expected_nested_diff.txt", "r") as f:
+        expected = f.read()
+
+    assert generate_diff(file1, file2) == expected
