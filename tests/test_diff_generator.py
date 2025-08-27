3244fdf629e02c1b3cbf5c40cc75fde969f17804
SlowKing <slowkinggg@vk.com>
2025-08-28 00:52:19 +0700

    test: :white_check_mark: add stylish and plain diff tests for JSON and YAML files

--- tests/test_diff_generator.py
+++ tests/test_diff_generator.py
@@ -1,31 +1,31 @@
 from gendiff import generate_diff
 
 
-def test_generate_diff_json():
+def test_generate_diff_json_stylish():
     file1 = "tests/test_data/file1.json"
     file2 = "tests/test_data/file2.json"
 
-    with open("tests/test_data/expected_diff.txt", "r") as f:
+    with open("tests/test_data/expected_diff_stylish.txt", "r") as f:
         expected = f.read()
 
     assert generate_diff(file1, file2) == expected
 
 
-def test_generate_diff_yaml():
+def test_generate_diff_yaml_stylish():
     file1 = "tests/test_data/file1.yml"
     file2 = "tests/test_data/file2.yml"
 
-    with open("tests/test_data/expected_diff.txt", "r") as f:
+    with open("tests/test_data/expected_diff_stylish.txt", "r") as f:
         expected = f.read()
 
     assert generate_diff(file1, file2) == expected
 
 
-def test_generate_diff_nested_json():
+def test_generate_diff_nested_json_stylish():
     file1 = "tests/test_data/file1_nested.json"
     file2 = "tests/test_data/file2_nested.json"
 
-    with open("tests/test_data/expected_nested_diff.txt", "r") as f:
+    with open("tests/test_data/expected_nested_diff_stylish.txt", "r") as f:
         expected = f.read()
 
     print(generate_diff(file1, file2))
@@ -34,11 +34,54 @@ def test_generate_diff_nested_json():
     assert generate_diff(file1, file2) == expected
 
 
-def test_generate_diff_nested_yaml():
+def test_generate_diff_nested_yaml_stylish():
     file1 = "tests/test_data/file1_nested.yml"
     file2 = "tests/test_data/file2_nested.yml"
 
-    with open("tests/test_data/expected_nested_diff.txt", "r") as f:
+    with open("tests/test_data/expected_nested_diff_stylish.txt", "r") as f:
         expected = f.read()
 
     assert generate_diff(file1, file2) == expected
+
+
+def test_generate_diff_json_plain():
+    file1 = "tests/test_data/file1.json"
+    file2 = "tests/test_data/file2.json"
+
+    with open("tests/test_data/expected_diff_plain.txt", "r") as f:
+        expected = f.read()
+
+    assert generate_diff(file1, file2, "plain") == expected
+
+
+def test_generate_diff_yaml_plain():
+    file1 = "tests/test_data/file1.yml"
+    file2 = "tests/test_data/file2.yml"
+
+    with open("tests/test_data/expected_diff_plain.txt", "r") as f:
+        expected = f.read()
+
+    assert generate_diff(file1, file2, "plain") == expected
+
+
+def test_generate_diff_nested_json_plain():
+    file1 = "tests/test_data/file1_nested.json"
+    file2 = "tests/test_data/file2_nested.json"
+
+    with open("tests/test_data/expected_nested_diff_plain.txt", "r") as f:
+        expected = f.read()
+
+    print(generate_diff(file1, file2, "plain"))
+    print(expected)
+
+    assert generate_diff(file1, file2, "plain") == expected
+
+
+def test_generate_diff_nested_yaml_plain():
+    file1 = "tests/test_data/file1_nested.yml"
+    file2 = "tests/test_data/file2_nested.yml"
+
+    with open("tests/test_data/expected_nested_diff_plain.txt", "r") as f:
+        expected = f.read()
+
+    assert generate_diff(file1, file2, "plain") == expected
