a8bbac806855e163155cba6bcd0f9f05da6e32db
SlowKing <slowkinggg@vk.com>
2025-09-02 16:17:00 +0700

    refactor #1: :recycle: consolidate test cases for stylish and plain formats in test_diff_generator and update test_data respectively

--- tests/test_diff_generator.py
+++ tests/test_diff_generator.py
@@ -1,87 +1,93 @@
+import os
+
+import pytest
+
 from gendiff import generate_diff
 
+STYLISH_FORMAT = 0
+PLAIN_FORMAT = 1
 
-def test_generate_diff_json_stylish():
-    file1 = "tests/test_data/file1.json"
-    file2 = "tests/test_data/file2.json"
+cases_plain = [
+    (
+        "tests/test_data/file1.json",
+        "tests/test_data/file2.json",
+        "stylish",
+        STYLISH_FORMAT,
+    ),
+    (
+        "tests/test_data/file1.json",
+        "tests/test_data/file2.json",
+        "plain",
+        PLAIN_FORMAT,
+    ),
+    (
+        "tests/test_data/file1.yml",
+        "tests/test_data/file2.yml",
+        "stylish",
+        STYLISH_FORMAT,
+    ),
+    (
+        "tests/test_data/file1.yml",
+        "tests/test_data/file2.yml",
+        "plain",
+        PLAIN_FORMAT,
+    ),
+]
 
-    with open("tests/test_data/expected_diff_stylish.txt", "r") as f:
-        expected = f.read()
-
-    assert generate_diff(file1, file2) == expected
-
-
-def test_generate_diff_yaml_stylish():
-    file1 = "tests/test_data/file1.yml"
-    file2 = "tests/test_data/file2.yml"
-
-    with open("tests/test_data/expected_diff_stylish.txt", "r") as f:
-        expected = f.read()
-
-    assert generate_diff(file1, file2) == expected
-
-
-def test_generate_diff_nested_json_stylish():
-    file1 = "tests/test_data/file1_nested.json"
-    file2 = "tests/test_data/file2_nested.json"
-
-    with open("tests/test_data/expected_nested_diff_stylish.txt", "r") as f:
-        expected = f.read()
-
-    print(generate_diff(file1, file2))
-    print(expected)
-
-    assert generate_diff(file1, file2) == expected
-
-
-def test_generate_diff_nested_yaml_stylish():
-    file1 = "tests/test_data/file1_nested.yml"
-    file2 = "tests/test_data/file2_nested.yml"
-
-    with open("tests/test_data/expected_nested_diff_stylish.txt", "r") as f:
-        expected = f.read()
-
-    assert generate_diff(file1, file2) == expected
-
-
-def test_generate_diff_json_plain():
-    file1 = "tests/test_data/file1.json"
-    file2 = "tests/test_data/file2.json"
-
-    with open("tests/test_data/expected_diff_plain.txt", "r") as f:
-        expected = f.read()
-
-    assert generate_diff(file1, file2, "plain") == expected
-
-
-def test_generate_diff_yaml_plain():
-    file1 = "tests/test_data/file1.yml"
-    file2 = "tests/test_data/file2.yml"
-
-    with open("tests/test_data/expected_diff_plain.txt", "r") as f:
-        expected = f.read()
-
-    assert generate_diff(file1, file2, "plain") == expected
+cases_nested = [
+    (
+        "tests/test_data/file1_nested.json",
+        "tests/test_data/file2_nested.json",
+        "stylish",
+        STYLISH_FORMAT,
+    ),
+    (
+        "tests/test_data/file1_nested.json",
+        "tests/test_data/file2_nested.json",
+        "plain",
+        PLAIN_FORMAT,
+    ),
+    (
+        "tests/test_data/file1_nested.yml",
+        "tests/test_data/file2_nested.yml",
+        "stylish",
+        STYLISH_FORMAT,
+    ),
+    (
+        "tests/test_data/file1_nested.yml",
+        "tests/test_data/file2_nested.yml",
+        "plain",
+        PLAIN_FORMAT,
+    ),
+]
 
 
-def test_generate_diff_nested_json_plain():
-    file1 = "tests/test_data/file1_nested.json"
-    file2 = "tests/test_data/file2_nested.json"
-
-    with open("tests/test_data/expected_nested_diff_plain.txt", "r") as f:
-        expected = f.read()
-
-    print(generate_diff(file1, file2, "plain"))
-    print(expected)
-
-    assert generate_diff(file1, file2, "plain") == expected
+def get_test_data_path(file_name):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(current_dir, "test_data", file_name)
 
 
-def test_generate_diff_nested_yaml_plain():
-    file1 = "tests/test_data/file1_nested.yml"
-    file2 = "tests/test_data/file2_nested.yml"
+def read(file_path):
+    with open(file_path, "r") as f:
+        result = f.read()
+    return result
 
-    with open("tests/test_data/expected_nested_diff_plain.txt", "r") as f:
-        expected = f.read()
 
-    assert generate_diff(file1, file2, "plain") == expected
+plain_expected_data = read(get_test_data_path("expected_plain.txt")).split(
+    "\n\n\n\n"
+)
+nested_expected_data = read(get_test_data_path("expected_nested.txt")).split(
+    "\n\n\n\n"
+)
+
+
+@pytest.mark.parametrize("file1,file2,format,format_index", cases_plain)
+def test_plain(file1, file2, format, format_index):
+    expected = plain_expected_data[format_index]
+    assert generate_diff(file1, file2, format) == expected
+
+
+@pytest.mark.parametrize("file1,file2,format,format_index", cases_nested)
+def test_nested(file1, file2, format, format_index):
+    expected = nested_expected_data[format_index]
+    assert generate_diff(file1, file2, format) == expected
